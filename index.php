f1285ab3fb5093723126db11852199c25d84b2e6
Vitalis Kwena <Vitalisk@users.noreply.github.com>
2022-07-07 11:08:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/tailwind.css" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div class="p-10">
+    <p class="ml-10">This is my first tailwindcss</p>
+  </div>
+</body>
+</html>
